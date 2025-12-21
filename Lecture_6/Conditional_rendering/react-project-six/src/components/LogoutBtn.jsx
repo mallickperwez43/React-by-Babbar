c98d00d7e5682260f339800888a0c0d813c14ad6
Perwez Mallick <mallickperwez43@gmail.com>
2025-12-21 21:35:12 +0530

    Create LogoutBtn.jsx

---
+++ Lecture_6/Conditional_rendering/react-project-six/src/components/LogoutBtn.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './LogoutBtn.css'
+
+const LogoutBtn = () => {
+    return (
+        <div>
+            <button id='logout-btn'>
+                Logout
+            </button>
+        </div>
+    )
+}
+
+export default LogoutBtn
