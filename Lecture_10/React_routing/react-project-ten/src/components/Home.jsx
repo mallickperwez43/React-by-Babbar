60bdc1a17f060e9966573f64430cddc6cd446ac1
Perwez Mallick <mallickperwez43@gmail.com>
2025-12-31 23:09:34 +0530

    Create Home.jsx

---
+++ Lecture_10/React_routing/react-project-ten/src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <section className="relative flex min-h-[calc(100vh-64px)] items-center justify-center bg-linear-to-br from-blue-50 to-white">
+
+            <div className="rounded-3xl bg-white/70 backdrop-blur-xl px-20 py-16 shadow-xl">
+                <h1 className="text-8xl font-black tracking-tight text-blue-500">
+                    Home
+                </h1>
+            </div>
+
+        </section>
+    )
+}
+
+export default Home
+
